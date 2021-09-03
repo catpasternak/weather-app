2ac680bbeb2ea823ce477cc28acdf500c48076de
cat <catpasternak@gmail.com>
2021-09-04 02:38:26 +0300

    Project ready

--- tests/test_geo_tools.py
+++ tests/test_geo_tools.py
@@ -9,6 +9,6 @@ def test_get_address_func_returns_correct_address_and_takes_reasonable_time():
     start = time.time()
     result = get_address(latitude, longitude, api_key=GEOCODE_API_KEY)
     execution_time = time.time() - start
-    assert result == 'Southwark Bridge Testing Station, Belvedere Place, Elephant and Castle, ' \
+    assert result == 'Southwark Bridge Testing Station, Belvedere Place, Elephant and Castle, '\
                      'London Borough of Southwark, London, Greater London, England, SE15, UK'
     assert execution_time < 2
